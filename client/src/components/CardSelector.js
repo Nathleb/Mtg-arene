6b24ea89d487af197e4c43c32e8c30210a63ca79
Nath <lebihannathan@gmail.com>
2021-10-05 21:52:38 +0200

    front calling card images

--- client/src/components/CardSelector.js
+++ client/src/components/CardSelector.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from "axios";
+
+const Card = (props) => (
+	<img src={props.card.image_uris.png} alt={"image" + props.card.name}></img> //png?normal?
+);
 
 class CardSelector extends Component {
-	state = {
-		cards: [],
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			cards: [],
+		};
+	}
 
-    componentDidMount(){
-        axios.get('http://localhost:5000/api/v1/cards/random/?limit=3&type=Legendary')
-        .then(result => {
-            console.log(result)
-        })
-    }
+	componentDidMount() {
+		axios
+			.get(
+				"http://localhost:5000/api/v1/cards/random/?limit=3&type=Legendary"
+			)
+			.then((result) => {
+				this.setState({ cards: result.data });
+			});
+		console.log(this.state);
+	}
+	CardSelector() {
+		return this.state.cards.map((currentcard) => {
+			return <Card card={currentcard} key={currentcard._id} />;
+		});
+	}
 
 	render() {
-		const { cards } = this.state;
 		return (
 			<div>
-				<p>Cardselector Component</p>
+				<h3>Pick 1</h3>
+				{this.CardSelector()}
 			</div>
 		);
 	}
