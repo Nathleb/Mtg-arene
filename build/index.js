db58906cca61cb2f234014e3c96722bfcbd92de4
Nath <lebihannathan@gmail.com>
2021-10-27 17:45:50 +0200

    change requested url

--- build/index.js
+++ build/index.js
@@ -15,18 +15,16 @@ const mongo = require("./db/conn");
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-app.use(
-	express.urlencoded({
-		extended: true,
-	})
-);
+app.use(express.urlencoded({
+  extended: true
+}));
 mongo();
 app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("./client/build"));
+  app.use(express.static("./../client/build"));
 }
 
 app.use("/api/v1/cards", routes.default.cards);
