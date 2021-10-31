36a3a26ec761f90ba56a9e7b9aed88788430712f
Nath <lebihannathan@gmail.com>
2021-10-31 22:47:04 +0100

    export de la liste

--- client/src/components/CardSelector.js
+++ client/src/components/CardSelector.js
@@ -17,6 +17,7 @@ class CardSelector extends Component {
 			enabled: true,
 		};
 		this.handleClick = this.handleClick.bind(this);
+		this.downloadList = this.downloadList.bind(this);
 	}
 
 	componentDidMount() {
@@ -89,26 +90,58 @@ class CardSelector extends Component {
 		}
 	}
 
+	downloadList() {
+		let list = Object.values(this.state.list).reduce((acc, curr) => {
+			return [...acc, ...curr];
+		}, []);
+		list = [...list, ...this.state.commander];
+		list = list.reduce((acc, curr) => {
+			if (acc[curr.name]) acc[curr.name]++;
+			else acc[curr.name] = 1;
+			return acc;
+		}, {});
+		let text = Object.keys(list).reduce((acc, curr) => {
+			acc = `${acc} ${list[curr]} ${curr}\n`;
+			return acc;
+		}, "");
+		let filename = `${this.state.commander[0].name} Deck`;
+		var element = document.createElement("a");
+		element.setAttribute(
+			"href",
+			"data:text/plain;charset=utf-8," + encodeURIComponent(text)
+		);
+		element.setAttribute("download", filename);
+
+		element.style.display = "none";
+		document.body.appendChild(element);
+
+		element.click();
+
+		document.body.removeChild(element);
+	}
+
 	CardSelector() {
-		return this.state.cards.map((currentcard) => {
-			return (
-				<span className="mytooltip">
-					<img
-						key={currentcard._id}
-						src={"data:image/jpg;base64," + currentcard.img}
-						alt={"image : " + currentcard.name}
-						className="img-fluid big-card"
-						onClick={() => this.handleClick(currentcard)}
-					></img>
-					<span className="tooltip-content">
+		if (this.state.pick < 30 || this.state.pick === "Leader") {
+			return this.state.cards.map((currentcard) => {
+				return (
+					<span className="mytooltip">
 						<img
+							key={currentcard._id}
 							src={"data:image/jpg;base64," + currentcard.img}
-							alt={"image too : " + currentcard.name}
+							alt={"image : " + currentcard.name}
+							className="img-fluid big-card"
+							onClick={() => this.handleClick(currentcard)}
 						></img>
+						<span className="tooltip-content img-fluid">
+							<img
+								src={"data:image/jpg;base64," + currentcard.img}
+								alt={"image too : " + currentcard.name}
+							></img>
+						</span>
 					</span>
-				</span>
-			);
-		});
+				);
+			});
+		}
 	}
 
 	CardList() {
@@ -156,7 +189,7 @@ class CardSelector extends Component {
 							alt={"image : " + currentcard.name}
 							className="img-fluid commander-card"
 						></img>
-						<span className="tooltip-content">
+						<span className="tooltip-content img-fluid">
 							<img
 								src={"data:image/jpg;base64," + currentcard.img}
 								alt={"image too : " + currentcard.name}
@@ -168,10 +201,25 @@ class CardSelector extends Component {
 		});
 	}
 
+	DownloadButton() {
+		if (this.state.pick === 30)
+			return (
+				<div className="sticky-bottom mt-5 mb-5">
+					<input
+						type="button"
+						className="btn-lg btn-primary"
+						value="Download list"
+						onClick={() => this.downloadList()}
+					/>
+				</div>
+			);
+	}
+
 	render() {
 		return (
 			<div className="d-flex section align-items-center justify-content-center mb-auto">
 				<h1 className="row mb-auto">{this.state.pick} / 30</h1>
+				<div>{this.DownloadButton()}</div>
 				<div className="mt-5 mb-5">{this.CardSelector()}</div>
 				<div className="d-flex cardlist">
 					{this.CardList()}
